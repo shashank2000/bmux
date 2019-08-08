9bff39e38fc71abf355c23e8ea69e5a7261ad9ba
Shashank Rammoorthy <sr22@stanford.edu>
2019-08-08 10:22:51 -0700

    added chrome script

--- rumpsapp.py
+++ rumpsapp.py
@@ -1,13 +1,61 @@
 import rumps
+from subprocess import Popen
 
 class AwesomeStatusBarApp(rumps.App):
     def __init__(self):
         super(AwesomeStatusBarApp, self).__init__("Awesome App")
-        self.menu = ["Preferences", "Silly button", "Say hi"]
+        self.menu = ["Start bmux session", "Run an apple script", "Say hi"]
 
-    @rumps.clicked("Preferences")
+    @rumps.clicked("Run an apple script")
     def prefs(self, _):
-        rumps.alert("jk! no preferences available!")
+        script= '''
+        tell Application "Safari" set myFile to open for access file (POSIX path of ((path to me as text) & "::") & "tabs.txt" as POSIX file) with write permission 
+        set windowNumber to 1 
+        set myTabs to every tab of window windowNumber
+        repeat the number of windows times
+    write "----- Window Number " & windowNumber & " -----\n\n" to myFile
+    set tabNumber to 0
+    repeat with aTab in myTabs
+
+      set tabTitle to "Name: " & name of aTab & "\n"
+      write tabTitle to myFile
+      set tabURL to "URL: " & URL of aTab & "\n\n"
+      write tabURL to myFile
+      set tabNumber to tabNumber + 1
+
+    end repeat
+
+    write "Window Number: " & windowNumber & " Number of tabs: " & tabNumber & "\n\n" to myFile
+    set windowNumber to windowNumber + 1
+    end repeat
+	close access myFile
+
+    end tell'''
+
+        chrome_script = '''
+        tell Application "Chrome" set myFile to open for access file (POSIX path of ((path to me as text) & "::") & "tabs.txt" as POSIX file) with append 
+        set windowNumber to 1 
+            set myTabs to every tab of window windowNumber
+        repeat the number of windows times
+        write "----- Window Number " & windowNumber & " -----\n\n" to myFile
+        set tabNumber to 0
+        repeat with aTab in myTabs
+
+        set tabTitle to "Name: " & name of aTab & "\n"
+         write tabTitle to myFile
+         set tabURL to "URL: " & URL of aTab & "\n\n"
+         write tabURL to myFile
+         set tabNumber to tabNumber + 1
+
+          end repeat
+
+            write "Window Number: " & windowNumber & " Number of tabs: " & tabNumber & "\n\n" to myFile
+            set windowNumber to windowNumber + 1
+        end repeat
+            close access myFile
+
+        end tell'''
+        p = Popen(['osascript', '-e', chrome_script])
 
     @rumps.clicked("Silly button")
     def onoff(self, sender):
