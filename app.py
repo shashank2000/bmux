9e65a264fc7a95bacec780b2643737b5cdc484c6
Lucas Pauker <lucas.pauker@gmail.com>
2019-08-16 16:58:07 -0700

    Working start session functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import rumps
-from subprocess import Popen
+import subprocess
 
 class StatusBarApp(rumps.App):
     def __init__(self):
@@ -9,7 +9,37 @@ class StatusBarApp(rumps.App):
         self.icon = "icon.png"
         self.temp_file = "temp_tabs.txt"
         self.tabs_file = "tabs.txt"
-        self.script = "safari_tabs.scpt"
+        self.script_file = "safari_tabs.scpt"
+
+    def read_sessions(self):
+        """Reads sessions from tabs_file. Returns a dictionary with keys as session names
+        and values as a list of URLs
+        """
+        sessions = {}
+        session_name = ""
+        urls = []
+        with open(self.tabs_file, "r") as f:
+            for line in f.readlines():
+                if line.startswith("Name") or line.isspace():
+                    if session_name:
+                        sessions[session_name] = urls
+                    session_name = " ".join(line.split(" ")[1:]).strip()
+                    urls = []
+                else:
+                    urls.append(line.strip())
+        return sessions
+
+    def write_sessions(self, sessions):
+        """Writes the passed dictionary into tabs_file
+        """
+        #open(self.tabs_file, "w").close()
+        with open(self.tabs_file, "w") as f:
+            for session_name in sessions:
+                urls = sessions[session_name]
+                f.write("Name: " + session_name + "\n")
+                for url in urls:
+                    f.write(url + "\n")
+                f.write("\n")
 
     @rumps.clicked("Start session")
     def record_tabs(self, _):
@@ -19,23 +49,38 @@ class StatusBarApp(rumps.App):
             dimensions=(300,20)
         ).run()
         if not response.clicked: return
+        session_name = response.text
+        session_name = ""
 
         open(self.temp_file, "w").close()
-        p = Popen(["osascript", self.script])
-        print("----Tabs recorded----")
+        subprocess.check_output(["osascript", self.script_file])
+        print("----Tabs found----")
 
         tabs = {}
-        with open(self.temp_file, "r") as read:
-            with open(self.tabs_file, "w") as write:
-                line = file.readline()
-                name = ""
-                if line == "Window":
-                    pass
-                elif line.startswith("Name"):
-                    name = line.split(" ")[1]
+        name = ""
+        with open(self.temp_file, "r") as temp_file:
+            for line in temp_file.readlines():
+                if line.startswith("Name"):
+                    name = " ".join(line.split(" ")[1:]).strip()
                 elif line.startswith("URL"):
-                    tabs[name] = line.split(" ")[1]
+                    tabs[name] = " ".join(line.split(" ")[1:]).strip()
 
+        sessions = self.read_sessions()
+        url_list = []
+        for tab_name in tabs:
+            url = tabs[tab_name]
+            url_list.append(url)
+        if not session_name:
+            base_string = "session_"
+            base_number = 1
+            while True:
+                session_name = base_string + str(base_number)
+                if session_name not in sessions: break
+                base_number += 1
+        sessions[session_name] = url_list
+
+        self.write_sessions(sessions)
+        print("----Tabs recorded----")
 
     def get_session_names(self):
         with open(self.tabs_file, "r") as f:
@@ -45,8 +90,8 @@ class StatusBarApp(rumps.App):
         @rumps.clicked("Load session", s)
         def load_session(self, _):
             pass
-
         # every 30 seconds, let us download all the open pages locally. Each time a tab is closed we delete the local copy of it. This is essentially browser tmux
         # LOOK AT RUMPS timer
+
 if __name__ == "__main__":
     StatusBarApp().run()
--- safari_tabs.scpt
+++ safari_tabs.scpt
@@ -1,18 +1,14 @@
 tell application "Safari"
 
   set myFile to open for access file (POSIX path of ((path to me as text) & "::") & "temp_tabs.txt" as POSIX file) with write permission
-  set windowNumber to 1
-  set myTabs to every tab of window windowNumber
-  repeat the number of windows times
-    write "Window\n" to myFile
-    repeat with aTab in myTabs
+  set myTabs to every tab of window 1
+  repeat with aTab in myTabs
 
-      set tabTitle to "Name: " & name of aTab & "\n"
-      write tabTitle to myFile
-      set tabURL to "URL: " & URL of aTab & "\n"
-      write tabURL to myFile
+    set tabTitle to "Name: " & name of aTab & "\n"
+    write tabTitle to myFile
+    set tabURL to "URL: " & URL of aTab & "\n"
+    write tabURL to myFile
 
-    end repeat
   end repeat
 	close access myFile
 
