b6b41fe57b047a9d998159700e32e1763e406750
Lucas Pauker <lucas.pauker@gmail.com>
2019-08-16 17:07:45 -0700

    Add delete sessions option and make load and delete display correct sessions

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ class StatusBarApp(rumps.App):
     def __init__(self):
         # add an icon by setting icon="filepath of icon"
         super(StatusBarApp, self).__init__("BMUX")
-        self.menu = ["Start session", "Load session"]
+        self.menu = ["Start session", "Load session", "Delete session"]
         self.icon = "icon.png"
         self.temp_file = "temp_tabs.txt"
         self.tabs_file = "tabs.txt"
@@ -82,16 +82,25 @@ class StatusBarApp(rumps.App):
         self.write_sessions(sessions)
         print("----Tabs recorded----")
 
-    def get_session_names(self):
-        with open(self.tabs_file, "r") as f:
-            pass #do something
-    session_names = ["foo", "bar"]
+    def get_session_names(tabs_file):
+        session_names = []
+        with open(tabs_file, "r") as f:
+            for line in f.readlines():
+                if line.startswith("Name"):
+                    session_names.append(" ".join(line.split(" ")[1:]).strip())
+        return session_names
+
+    session_names = get_session_names("tabs.txt") # Hack solution
     for s in session_names:
         @rumps.clicked("Load session", s)
         def load_session(self, _):
             pass
         # every 30 seconds, let us download all the open pages locally. Each time a tab is closed we delete the local copy of it. This is essentially browser tmux
         # LOOK AT RUMPS timer
+
+        @rumps.clicked("Delete session", s)
+        def delete_tabs(self, _):
+            pass
 
 if __name__ == "__main__":
     StatusBarApp().run()
